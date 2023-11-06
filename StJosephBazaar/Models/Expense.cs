c7c131943cb8de61602622b4a9c7d2ae630a35f5
JP Lyon <japylyon@gmail.com>
2023-11-06 12:12:44 -0600

    Added ID to Income + Expenses Models

--- StJosephBazaar/Models/Expense.cs
+++ StJosephBazaar/Models/Expense.cs
@@ -6,6 +6,7 @@ public class Expense
 {
     [Key]
     public int BoothID { get; set; }
+    public int ID { get; set; }
     public DateOnly Date { get; set; }
     public string? Description { get; set; }
     public decimal Total { get; set;}
--- StJosephBazaar/Models/Income.cs
+++ StJosephBazaar/Models/Income.cs
@@ -6,6 +6,7 @@ public class Income
 {
     [Key]
     public int BoothID { get; set; }
+    public int ID { get; set; }
     public DateOnly Date { get; set; }
     public TimeOnly HourCollected { get; set; }
     public decimal Total { get; set; }
