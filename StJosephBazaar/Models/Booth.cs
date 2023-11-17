36d4ccf93e2d01cbeb9225a5c295b41d1c7f7e55
Caden McCarty <mccartycl@hendrix.edu>
2023-11-17 12:37:34 -0600

    Booth Updates

--- StJosephBazaar/Models/Booth.cs
+++ StJosephBazaar/Models/Booth.cs
@@ -6,7 +6,11 @@ public class Booth
 {
     public int Id { get; set; }
     public string? Name { get; set; }
-    public decimal StartupTotal { get; set; }
+    // public decimal StartupTotal { get; set; }
+    public decimal Friday { get; set; }
+    public decimal Saturday { get; set; }
+    public decimal Auction { get; set; }
+    public decimal Gross_Revenue { get; set; }
     public ICollection<Expense>? Expenses { get; set;}
-    public ICollection<Income>? Incomes { get; set;}
+    public ICollection<Income>? Net_Incomes { get; set;}
 }
