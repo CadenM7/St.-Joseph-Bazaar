99c97635cc0e830f30740360a8d08fa4708c473f
JP Lyon <japylyon@gmail.com>
2023-11-18 09:59:11 -0600

    Expense Page Filter Added

--- StJosephBazaar/Pages/Expenses/Index.cshtml.cs
+++ StJosephBazaar/Pages/Expenses/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using StJosephBazaar.Data;
 using StJosephBazaar.Models;
@@ -21,12 +22,30 @@ namespace StJosephBazaar.Pages.Expenses
 
         public IList<Expense> Expense { get;set; } = default!;
 
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+        public SelectList? Names { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? BoothName { get; set; }
+
         public async Task OnGetAsync()
         {
             if (_context.Expense != null)
             {
                 Expense = await _context.Expense
                 .Include(e => e.Booth).ToListAsync();
+            
+            
+            var expenses = from m in _context.Expense
+                                        select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                expenses = expenses.Where(s => s.Booth.Name.Contains(SearchString));
+            }
+
+                Expense = await expenses.ToListAsync();
             }
         }
     }
