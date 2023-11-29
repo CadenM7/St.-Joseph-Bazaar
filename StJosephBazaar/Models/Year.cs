8fd26513977b739dd0bbc88405e391ac60aef444
TMGBNoodle <grovesbenedicttm@hendrix.edu>
2023-11-29 12:32:57 -0600

    Font change and some infraastructure changes

--- StJosephBazaar/Models/Year.cs
+++ StJosephBazaar/Models/Year.cs
@@ -5,6 +5,7 @@ namespace StJosephBazaar.Models;
 public class Year
 {
     public int YearID{get;set;}
+
     public int YearVal{get;set;}
     public ICollection<Booth>? Booths { get; set; }
 }
--- StJosephBazaar/Pages/Booths/Create.cshtml.cs
+++ StJosephBazaar/Pages/Booths/Create.cshtml.cs
@@ -7,10 +7,12 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using StJosephBazaar.Data;
 using StJosephBazaar.Models;
+using StJosephBazaar.Pages.Booths;
+using StJosephBazaar.Pages.Courses;
 
 namespace StJosephBazaar.Pages.Booths
 {
-    public class CreateModel : PageModel
+    public class CreateModel : YearPageModel
     {
         private readonly StJosephBazaar.Data.BazaarContext _context;
 
@@ -21,6 +23,7 @@ namespace StJosephBazaar.Pages.Booths
 
         public IActionResult OnGet()
         {
+            PopulateYearDropDownList(_context);
             return Page();
         }
 
@@ -31,15 +34,20 @@ namespace StJosephBazaar.Pages.Booths
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
-          if (!ModelState.IsValid || _context.Booth == null || Booth == null)
-            {
-                return Page();
-            }
+            var emptyBooth = new Booth();
+                if(await TryUpdateModelAsync<Booth>(
+                        emptyBooth,
+                        "booth",
+                        s => s.Id, s=> s.Name, s => s.Friday, s => s.Year, s => s.Saturday, s => s.Auction, s => s.Gross_Revenue, s => s.Purchases, s => s.Expenses, s => s.Income, s => s.Net_Income))
 
-            _context.Booth.Add(Booth);
-            await _context.SaveChangesAsync();
+                    {
+                        _context.Booth.Add(emptyBooth);
+                        await _context.SaveChangesAsync();
+                        return RedirectToPage("./Index");
+                    }
 
-            return RedirectToPage("./Index");
-        }
+                    PopulateYearDropDownList(_context, emptyBooth.Year);
+                    return Page();
+                }
     }
 }
